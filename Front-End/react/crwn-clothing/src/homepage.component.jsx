99c1f53dc4676817bfe1fbef3298fb57f96a4ae2
Temitope <omiwoletemitope@gmail.com>
2022-02-08 22:22:04 +0100

    create the project with create react app and added a component

---
+++ Front-End/react/crwn-clothing/src/homepage.component.jsx
@@ -0,0 +1,46 @@
+import react from React;
+
+const Homepage = () => {
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>HATS</div>
+                    <span className='subtitle'>SHOPS NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>JACKETS</div>
+                    <span className='subtitle'>SHOPS NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>SNEAKERS</div>
+                    <span className='subtitle'>SHOPS NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>WOMENS</div>
+                    <span className='subtitle'>SHOPS NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>MEN</div>
+                    <span className='subtitle'>SHOPS NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+}
