aa4105f4454418c0a8ec1f3cb1ed47a83107e0a7
Temitope <omiwoletemitope@gmail.com>
2022-02-10 00:27:21 +0100

    broken our homepage into smaller components, finished styling our menu-item

--- Front-End/react/crwn-clothing/src/App.js
+++ Front-End/react/crwn-clothing/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Homepage from "./pages/homepage/homepage.component";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
